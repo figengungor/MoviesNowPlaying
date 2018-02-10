6eae47a39433469f2854e026b2b126534055427c
figengungor <figengungor1@gmail.com>
2018-02-10 15:42:52 +0300

    Add share feature

--- app/src/main/java/com/figengungor/moviesnowplaying/MovieDetailActivity.java
+++ app/src/main/java/com/figengungor/moviesnowplaying/MovieDetailActivity.java
@@ -1,5 +1,6 @@
 package com.figengungor.moviesnowplaying;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.databinding.DataBindingUtil;
 import android.net.Uri;
@@ -9,6 +10,7 @@ import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.figengungor.moviesnowplaying.data.MovieContract;
 import com.figengungor.moviesnowplaying.databinding.ActivityMovieDetailBinding;
@@ -47,14 +49,33 @@ public class MovieDetailActivity extends AppCompatActivity implements LoaderMana
 
         mDetailBinding = DataBindingUtil.setContentView(this, R.layout.activity_movie_detail);
 
-        if(getSupportActionBar()!=null) {
+        if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
         mUri = getIntent().getData();
-        if(mUri==null) throw new NullPointerException("Uri for MovieDetailActivity cannot be null");
+        if (mUri == null)
+            throw new NullPointerException("Uri for MovieDetailActivity cannot be null");
 
         getSupportLoaderManager().initLoader(ID_MOVIE_DETAIL_LOADER, null, this);
+
+        mDetailBinding.shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mDetailBinding.shareButton.getTag() != null)
+                    shareMovie((int) mDetailBinding.shareButton.getTag());
+            }
+        });
+
+
+    }
+
+    private void shareMovie(int movieId) {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "https://www.themoviedb.org/movie/" + movieId);
+        sendIntent.setType("text/plain");
+        startActivity(Intent.createChooser(sendIntent, getString(R.string.choosesharingplatform)));
     }
 
     @Override
@@ -62,7 +83,7 @@ public class MovieDetailActivity extends AppCompatActivity implements LoaderMana
         switch (loaderId) {
             case ID_MOVIE_DETAIL_LOADER:
 
-                return  new CursorLoader(
+                return new CursorLoader(
                         this,
                         mUri,
                         MOVIE_DETAIL_PROJECTION,
@@ -71,20 +92,23 @@ public class MovieDetailActivity extends AppCompatActivity implements LoaderMana
                         null
                 );
             default:
-                throw new RuntimeException("Loader Not Implemented: "+ loaderId);
+                throw new RuntimeException("Loader Not Implemented: " + loaderId);
         }
     }
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
         boolean cursorHasValidData = false;
-        if(data!=null && data.moveToFirst()) {
+        if (data != null && data.moveToFirst()) {
             cursorHasValidData = true;
         }
 
-        if(!cursorHasValidData) {
+        if (!cursorHasValidData) {
             return;
         }
+
+        int movieId = data.getInt(INDEX_MOVIE_ID);
+        mDetailBinding.shareButton.setTag(movieId);
 
         String posterPath = data.getString(INDEX_MOVIE_POSTER_PATH);
         ImageUtils.loadImageUrl(posterPath, mDetailBinding.imageViewPoster, ImageUtils.ImageType.POSTER);
